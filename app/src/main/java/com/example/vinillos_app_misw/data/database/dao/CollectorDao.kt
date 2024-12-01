934f367851a67822963a4609bb5e3edc536f64a9
Felipev94 <felipevi94.gil@gmail.com>
2024-11-30 20:00:11 -0500

    Implementacion de Servicios y persistencia local collecionistas

--- app/src/main/java/com/example/vinillos_app_misw/data/database/dao/CollectorDao.kt
+++ app/src/main/java/com/example/vinillos_app_misw/data/database/dao/CollectorDao.kt
@@ -2,22 +2,54 @@ package com.example.vinillos_app_misw.data.database.dao
 
 import androidx.room.*
 import com.example.vinillos_app_misw.data.model.Collector
-import com.example.vinillos_app_misw.data.model.CollectorWithAlbums
+import com.example.vinillos_app_misw.data.model.CollectorAlbum
+import com.example.vinillos_app_misw.data.model.CollectorWithDetails
+import com.example.vinillos_app_misw.data.model.Comment
+import com.example.vinillos_app_misw.data.model.Performer
 
 @Dao
 interface CollectorDao {
-    @Query("SELECT * FROM collector_table")
-    suspend fun getCollectors(): List<Collector>
-
-    @Query("SELECT * FROM collector_table WHERE id = :id")
-    suspend fun getCollector(id: Int): Collector?
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertCollector(collector: Collector)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertCollectors(collectors: List<Collector>)
+    suspend fun insertPerformer(performer: Performer)
 
-    @Delete
-    suspend fun deleteCollector(collector: Collector)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCollectorAlbum(collectorAlbum: CollectorAlbum)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertComment(comment: Comment)
+
+
+    @Query("SELECT * FROM collector_table")
+    suspend fun getCollectors(): List<CollectorWithDetails>
+
+    @Query("SELECT * FROM collector_table WHERE id = :id")
+    suspend fun getCollector(id: Int): CollectorWithDetails?
+
+    @Transaction
+    suspend fun insertCollectorWithDetails(collectorWithDetails: CollectorWithDetails) {
+        insertCollector(collectorWithDetails.collector)
+
+        collectorWithDetails.collectorAlbums.forEach {
+            insertCollectorAlbum(it)
+        }
+
+        collectorWithDetails.favoritePerformers .forEach {
+            insertPerformer(it)
+        }
+
+        collectorWithDetails.comments.forEach {
+            insertComment(it)
+        }
+    }
+
+    @Transaction
+    suspend fun insertCollectorsWithDetails(collectorsWithDetails: List<CollectorWithDetails>) {
+        for (collectorWithDetails in collectorsWithDetails) {
+            insertCollectorWithDetails(collectorWithDetails)
+        }
+    }
 }
